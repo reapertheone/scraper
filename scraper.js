e61427b42aa4134bd040107d085951c8c1e9f091
reapertheone <tutrai.gergo01@gmail.com>
2021-01-27 23:31:27 +0000

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -2,11 +2,15 @@ const {JSDOM} = require('jsdom');
 const fetch = require('node-fetch');
 
 const fetching=async ()=>{
+    const start=new Date()
     const http=await fetch('https://weight-tracker2.herokuapp.com/login')
     const text=await http.text()
     const dom=new JSDOM(text)     
-    console.log(dom.window.document.querySelector('h1').textContent)      
+    console.log(dom.window.document.querySelector('h1').textContent)
+    console.log((new Date().getTime()-start.getTime())/1000,'s runtime')      
 
 }
 
+
 fetching()
+

8c8965de98cee40d94d6c224aa6ecbcae4c7a692
reapertheone <tutrai.gergo01@gmail.com>
2021-01-28 12:19:53 +0000

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,13 +3,19 @@ const fetch = require('node-fetch');
 
 const fetching=async ()=>{
     const start=new Date()
-    const http=await fetch('https://www.google.com/search?q=gumikacsa')
+    const regex=new RegExp('[0-9]')
+    console.log(regex)
+    const http=await fetch('https://ingatlan.com/lista/kiado+v-ker+lakas?fbclid=IwAR0itofsHolv370TKF1--pWP6DUkRkCfrsS1_1lltHizDx4xiaCeFdYR0js')
     const text=await http.text()
     const dom=new JSDOM(text)     
-    const hrefs=dom.window.document.querySelectorAll('a')
-    for(let a of hrefs){
-        console.log(a.href)
-    }
+    const addresses=dom.window.document.querySelectorAll('.listing__address')
+    
+   
+    
+     for(let address of addresses){
+        const toCheck=address.textContent.split(',')[0]
+        regex.test(toCheck)? console.log(toCheck): console.log('not contains number')
+    } 
     console.log((new Date().getTime()-start.getTime())/1000,'s runtime')      
 
 }

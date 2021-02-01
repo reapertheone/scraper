0156565e291601041ea284b40dc86662e29e5c0a
reapertheone <tutrai.gergo01@gmail.com>
2021-02-01 01:36:18 +0000

    saving to json and disable data duplication

--- scraper.js
+++ scraper.js
@@ -14,43 +14,44 @@ const fetching=async (url)=>{
     const start=new Date()
     const addressArray=[]
     const regex=new RegExp('[0-9]')
-    //console.log(regex)
     const pages=await getPages(url)
-    //console.log(pages)
+    //if you want to check it with partial data change pages to a number<=10 to make sure it wont cause error
     for(let i=1;i<=pages;i++){
         const http=await fetch(`${url}?page=${i}`)
         const text=await http.text()
         const dom=new JSDOM(text)     
         const addresses=dom.window.document.querySelectorAll('.listing__address')
-        //console.log(addresses)
         for(let address of addresses){
             const toCheck=address.textContent
-            if(regex.test(toCheck)){
+            if(regex.test(toCheck)&&!addressArray.includes(toCheck)){
+               
                 addressArray.push(toCheck)
                 }
    
             } 
             console.log(i)
         }
-    
+        console.log((new Date().getTime()-start.getTime())/1000,'s runtime')  
     return addressArray
-    console.log((new Date().getTime()-start.getTime())/1000,'s runtime')      
+        
 
 }
 
-//&page=2
-
-//fetching()
-const fetchAndSave = async ()=>{
-    let res=await fetching('https://ingatlan.com/szukites/kiado+lakas+vi-ker')
-    let file=fs.createWriteStream('./asdhatodik.txt')
-    res.forEach((address)=>{
-        file.write(`${address},\n`)
+//save addresses to json
+const fetchAndSave = async (url)=>{
+    let res=await fetching(url)
+    let data=JSON.stringify(res)
+    fs.writeFile('./asd.json',data.split('\",').join('\",\n'),(err)=>{
+        if(err) throw err;
+        console.log('done')
     })
+    
 
 }
 
-fetchAndSave() 
+
+//change this url to fetch different page
+fetchAndSave('https://ingatlan.com/lista/kiado+v-ker+lakas') 
 
 //811 pages-692.431 sec=13 min
 //2166 pages-1860.018=31 min
